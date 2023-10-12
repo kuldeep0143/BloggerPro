f3f91cc95ddca367eba2aca6297e8b78b901faf9
Kuldeep <kuldeepchouhan923@gmail.com>
2023-10-12 13:10:30 +0530

    Initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logo = ({width = '20px'}) => {
+  return (
+    <div>
+      
+    <img href="/" className="h-16 mx-2" src={"https://www.freeiconspng.com/thumbs/blogger-logo-icon-png/blogger-logo-icon-png-29.png"} alt="My logo" />
+  
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react'
+import appwriteService from "../appwrite/config";
+import {Container, PostCard} from '../components'
+
+function Home() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        appwriteService.getPosts().then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
+    }, [])
+  
+    if (posts.length === 0) {
+        return (
+            <div className="w-full py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex flex-wrap text-gray-900 cursor-pointer">
+                        <div className="p-2 w-full">
+                            <h1 className="text-3xl font-bold hover:text-gray-500">
+                            Welcome to BloggerPro - Where Your Ideas Become Stories.
+                            </h1>
+                            <h2 className="text-2xl font-bold hover:text-gray-500 p-5 ">
+                                Login to read posts</h2>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Home
